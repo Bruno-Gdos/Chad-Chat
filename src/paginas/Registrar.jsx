57acccd273edd8d39dfb51781c1a8cb10417b4a7
Lucas Andrade <lucas.andrade7899@gmail.com>
2022-11-03 15:32:57 -0300

    Integração com Firebase e Cadastro de Conta

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import Add from "../img/addAvatar.png"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { useState } from 'react';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
 
-export const Registrar = () => {
+const Registrar = () => {
+  const [err, setErr] = useState(false)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+
+    try{
+      const res = createUserWithEmailAndPassword(auth, email, password)
+
+  const storageRef = ref(storage, 'images/rivers.jpg');
+
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+      (error) => {
+        setErr(true)
+      }, 
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          await updateProfile(res.user, {
+            displayName,
+            photoURL:downloadURL,
+          })
+          await setDoc(doc(db, "users", res.user.uid), {
+            uid: res.user.uid,
+            displayName,
+            email,
+            photoURL: downloadURL
+          })
+        });
+      }
+    );
+
+    } catch (err){
+      setErr(true)
+    }
+  }
+
   return (
     <div className='formContainer'>
         <div className='formWrapper'>
             <span className='logo'>Chad Chat</span>
             <span className='title'>Registrar</span>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Nome'/>
                 <input type="email" placeholder='E-mail'/>
                 <input type="password" placeholder='Senha'/>
@@ -17,9 +62,12 @@ export const Registrar = () => {
                   <span>Adicione uma imagem de perfil</span>
                 </label>
                 <button>Registrar</button>
+                {err && <span>Algo deu errado</span>}
             </form>
             <p>Já tem uma conta? Fazer Login</p>
         </div>
     </div>
   )
 }
+
+export default Registrar
